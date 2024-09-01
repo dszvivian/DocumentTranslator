5cb3249ee70860d8134fee5c515329fb77fe5297
dszvivian <dszvivian@gmail.com>
2024-09-01 23:54:58 +0530

    Created Workflow

--- document_creation.py
+++ document_creation.py
@@ -15,24 +15,36 @@ class DocumentCreation(DocumentProcessor):
 
         for page in self.doc:
 
-            self.new_doc.insert_page(-1,
-                                  text = page.get_text(),
-                                  width = 595,
-                                  height = 842
+            current_page = self.new_doc.new_page(-1,
+                                  width = page.rect.width,
+                                  height = page.rect.height
                                   )
             
+            # Process the Text Blocks
 
-            
+            # Gets the text blocks and all its meta data in a Dictionary
+            for block in page.get_text("dict")["blocks"]:
+                if block["type"] == 0:  # type == 0 --> Textblock 1 -->Image Block ... etc....
+                    for line in block["lines"]:
+                        for span in line["spans"]:   # span is a contioguous sequence of characters with same font and style
+                            original_text = span["text"]
+                            font_size = span["size"]
+                            color = span["color"]
+                            origin = span["origin"]
+                            font = span["font"]
+
+                            print(f"font={font}  fsize={font_size}")
+
+                            current_page.insert_text(
+                                origin,
+                                original_text,
+                                # fontname=font,
+                                fontsize=font_size
+                                # color=color
+                            )                    
+
+
+
 
     def save_new_doc(self):
         self.new_doc.save('test_translated.pdf')
-
-
-
-    
-
-    
-
-    
-     
-
