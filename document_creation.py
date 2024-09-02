b7af1b47fc772a0e5ec379330dff6918c881b499
dszvivian <dszvivian@gmail.com>
2024-09-02 23:53:22 +0530

    Translates Images, Text and creates a new pdf with translated Text

--- document_creation.py
+++ document_creation.py
@@ -1,5 +1,6 @@
+import io
 import pymupdf
-
+from PIL import Image
 from document_processor import DocumentProcessor
 
 
@@ -18,12 +19,66 @@ class DocumentCreation(DocumentProcessor):
             current_page = self.new_doc.new_page(-1,
                                   width = page.rect.width,
                                   height = page.rect.height
-                                  )
+                                  ) 
+
+            # Process Images
+            image_list = page.get_images(full=True)
+
+            # for img_index, img in enumerate(image_list):
+
+            #     # xref = img[0]
+            #     # base_image = self.doc.extract_image(xref)
+
+            #     print("    ------------------     ")
+
+
+            #     xref = img[0]
+            #     base_image = self.doc.extract_image(xref)
+            #     image_bytes =  base_image["image"]
+
+
+                
+            #     # print(image_bytes)
+
+            #     Image.open(io.BytesIO(image_bytes))
+
+
+            #     print("    ------------------     ")
+            #     break 
+
+
+
             
-            # Process the Text Blocks
 
             # Gets the text blocks and all its meta data in a Dictionary
             for block in page.get_text("dict")["blocks"]:
+
+                # Process the Image Block
+                if block["type"] == 1:
+                    image = Image.open(io.BytesIO(block["image"]))
+
+                    try:
+
+                        translated_image_bytes = self.translate_image(image)
+
+                    except Exception as e:
+                        print("Error while Translating image")
+                        print("Replaced with Original Image")
+
+                        translated_image_bytes = block["image"]
+
+
+                    # Insert the modified Image
+                    image_rect =  block["bbox"]
+                    current_page.insert_image(
+                        image_rect,
+                        stream=translated_image_bytes
+                        )
+
+
+
+
+                # Process the Text Blocks
                 if block["type"] == 0:  # type == 0 --> Textblock 1 -->Image Block ... etc....
                     for line in block["lines"]:
                         for span in line["spans"]:   # span is a contioguous sequence of characters with same font and style
@@ -33,11 +88,23 @@ class DocumentCreation(DocumentProcessor):
                             origin = span["origin"]
                             font = span["font"]
 
-                            print(f"font={font}  fsize={font_size}")
+                            try:
+                                translated_text = self.translator.translate(
+                                    original_text
+                                )
+                            except Exception as e:
+                                print(f"Error translating text(ie: So replaced with original text)")
+                                print(e)
+                                translated_text = original_text
 
+
+                            print(translated_text)
+
+
+                            # todo: Fix font and color
                             current_page.insert_text(
                                 origin,
-                                original_text,
+                                translated_text,
                                 # fontname=font,
                                 fontsize=font_size
                                 # color=color
--- document_processor.py
+++ document_processor.py
@@ -2,7 +2,7 @@ import io
 import pymupdf
 from ocr_engine import OcrEngine
 from translator import LanguageTranslator
-from PIL import Image 
+from PIL import Image, ImageDraw, ImageFont
 
 
 class DocumentProcessor:
@@ -10,9 +10,6 @@ class DocumentProcessor:
     # def __init__(self,path) -> None:
     #     self.reader = PdfReader(path)
     #     self.n_pages = len(self.reader.pages)
-
-    #     self.translator = LanguageTranslator() 
-    #     self.ocr_engine = OcrEngine()       
 
     # def translate_page(self,page_number):
     #     page =  self.reader.pages[page_number]
@@ -40,11 +37,46 @@ class DocumentProcessor:
 
     def __init__(self,path):
         self.doc = pymupdf.open(path) 
+        self.translator = LanguageTranslator() 
+        self.ocr_engine = OcrEngine()       
+
 
     def translate_page(page): 
         pass
 
-    def translate_image(page):
-        pass
+    def translate_image(self,image):
+        """
+        returns translated Image Bytes
+        """
+
+        # Extract and Translate the text from image
+        extracted_text = self.ocr_engine.process_image(image)
+
+        # translated_text = extracted_text
+        translated_text = self.translator.translate(extracted_text)
+
+        # Create a New Image with the translated Text
+        draw =  ImageDraw.Draw(image)
+        font = ImageFont.load_default() 
+        draw.text(
+            (10,10),
+            translated_text,
+            font=font,
+            fill=(255,0,0)  # Red Text for Visibility
+            )
+        
+        # Convert image back to Bytes 
+        img_bytes_arr = io.BytesIO()
+        image.save(img_bytes_arr, format='PNG')
+        img_bytes_arr = img_bytes_arr.getvalue()  # convert to Bytes
+
+        return img_bytes_arr  
+
+
+
+
+
+
+        
 
 
