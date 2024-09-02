74e43e2e6fe0586c42c53087ec8836f00261319b
dszvivian <dszvivian@gmail.com>
2024-09-02 23:58:55 +0530

    Refactor

--- main.py
+++ main.py
@@ -8,14 +8,5 @@ doc.process_pages()
 doc.save_new_doc()
 
 
-# from document_processor import DocumentProcessor
-# from translator import Translator
-
-# reader =  DocumentProcessor('./Input_Test_PDF1.pdf')
-
-
-# reader.extract_all_text()
-
-
 
 
